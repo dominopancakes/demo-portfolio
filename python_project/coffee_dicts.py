697ea2199aada1e3f7a2ab47d60d039e2b7585d4
Antony <74658234+dominopancakes@users.noreply.github.com>
2025-05-12 14:28:54 +0100

    initial commit

---
+++ python_project/coffee_dicts.py
@@ -0,0 +1,104 @@
+books = {
+    "Cujo" : {
+        "Author" : "Stephen King",
+        "Genre" : "Horror",
+        "Price" : 9.99,
+        "Stock" : 5
+    },
+    "1984" : {
+        "Author" : "George Orwell",
+        "Genre" : "Prophecy",
+        "Price" : 19.84,
+        "Stock" : 5
+    },
+    "Red Dragon" : {
+        "Author" : "Thomas Harris",
+        "Genre" : "Thriller",
+        "Price" : 9.99,
+        "Stock" : 5
+    }
+}
+
+coffee = {
+    "Latte" : {
+        "Description" : "espresso with hot milk",
+        "Price" : 2.99,
+        "Stock" : 30
+    },
+    "Cappucino" : {
+        "Description" : "espresso with hot milk and foam",
+        "Price" : 3.99,
+        "Stock" : 30
+    },
+    "Mocha" : {
+        "Description" : "espresso with hot milk and chocolate",
+        "Price" : 4.99,
+        "Stock" : 30
+    },
+    "Custom Coffee" : {
+        "Description" : "n/a",
+        "Price" : 0,
+        "Stock" : 0
+    }
+}
+
+custom_coffee_prices = {
+    "espresso" : 2.50,
+    "steamed milk" : 1.00,
+    "foam\t" : 0.50,
+    "chocolate" : 1.00,
+    "caramel" : 1.00,
+    "vanilla" : 1.00,
+    "hot water" : 0.2
+}
+
+employees = {
+    "Ant" : "1234",
+    "Frankie" : "3456",
+    "Scout" : "5678"
+}
+
+food_items = {
+    "Chocolate Muffin" : {
+        "Type" : "muffin",
+        "Description" : "Chocolate sponge & chocolate chips topped with chocolate ganache",
+        "Ingredients" : ["flour","eggs","milk","butter","cocoa"],
+        "Price" : 3.50,
+        "Stock" : 10
+    },
+    "Blueberry Muffin" : {
+        "Type" : "muffin",
+        "Description" : "Vanilla sponge & blueberries topped with buttercream",
+        "Ingredients" : ["flour","eggs","milk","butter","blueberries"],
+        "Price" : 3.50,
+        "Stock" : 10
+    },
+    "Victoria Sponge" : {
+        "Type" : "cake",
+        "Description" : "Chocolate sponge, jam, & cream",
+        "Ingredients" : ["flour","eggs","milk","butter","jam"],
+        "Price" : 3.00,
+        "Stock" : 10
+    },
+    "Red Velvet" : {
+        "Type" : "cake",
+        "Description" : "Chocolate sponge with buttercream layers and wrap",
+        "Ingredients" : ["flour","eggs","milk","butter","cocoa", "Red food colouring"],
+        "Price" : 3.00,
+        "Stock" : 10
+    },
+    "Ham Sandwich" : {
+        "Type" : "sandwich",
+        "Description" : "Ham between buttered white bread",
+        "Ingredients" : ["butter","bread","ham"],
+        "Price" : 3.00,
+        "Stock" : 10
+    },
+    "Cheese Sandwich" : {
+        "Type" : "sandwich",
+        "Description" : "Cheese between buttered white bread",
+        "Ingredients" : ["butter","bread","Cheese"],
+        "Price" : 3.00,
+        "Stock" : 10
+    }
+}
